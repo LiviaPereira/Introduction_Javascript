02ce118265a349eff663f35ddb6714c6cdf97bec
Livia <liviapereira1997@hotmail.com>
2020-07-09 16:04:11 -0300

    practicing javascript

--- js/main.js
+++ js/main.js
@@ -1,16 +1,44 @@
-var nome = "Lívia Pereira";
-var n1 = 23;
-var n2 = 10;
-var frase = "Japão é o melhor time do mundo";
+
+var frutas = [{nome:"maçã", cor:"vermelha"}, {nome:"uva", cor:"roxa"}]
+console.log(frutas);
+alert(frutas[1].nome);
 
 
 
-//alert(nome + " tem " + idade + " anos");
 
-//alert(idade + idade2);
 
-console.log(nome);
-console.log(n1 + n2);
-//console.log(frase.replace("Japão", "Brasil"));
-//console.log(frase.toUpperCase());
-console.log(frase.toLowerCase());
+// var fruta = {nome:"maçã", cor:"vermelha"}
+// console.log(fruta.nome);
+// alert(fruta.cor);
+
+
+
+
+// var lista = ["maça", "pera", "laranja"];
+// //lista.push("uva");
+// //lista.pop();
+// //console.log(lista.length);
+
+// console.log(lista);
+// console.log(lista.toString());
+// console.log(lista.join(" - "));
+
+
+
+
+// var nome = "Lívia Pereira";
+// var n1 = 23;
+// var n2 = 10;
+// var frase = "Japão é o melhor time do mundo";
+
+
+
+// //alert(nome + " tem " + idade + " anos");
+
+// //alert(idade + idade2);
+
+// console.log(nome);
+// console.log(n1 + n2);
+// //console.log(frase.replace("Japão", "Brasil"));
+// //console.log(frase.toUpperCase());
+// console.log(frase.toLowerCase());
