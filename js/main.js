3c6cbb99f5fbe00386e40d8cc44f14b5dd6f5493
Livia <liviapereira1997@hotmail.com>
2020-07-09 16:24:04 -0300

    practicing some more coding using javascript

--- js/main.js
+++ js/main.js
@@ -1,17 +1,47 @@
 
-var frutas = [{nome:"maçã", cor:"vermelha"}, {nome:"uva", cor:"roxa"}]
-console.log(frutas);
-alert(frutas[1].nome);
+var d = new Date();
+//alert(d.getMonth()+1);
+//alert(d.getMinutes());
+//alert(d.getDay());
+alert(d.getHours());
 
 
 
+
+
+// var count;
+// for(count=0; count <= 5; count++){
+//     alert (count);
+// }
+
+
+// var count = 0;
+// while (count <= 5){
+//     console.log(count);
+//     alert(count);
+//     //count = count + 1;
+//     count++;
+// }
+
+
+
+// var idade = prompt("Qual sua idade?");
+// //var idade = 18;
+// if(idade >= 18){
+//     alert("maior de idade");
+// } else {
+//     alert("menor de idade")
+// };
+
+
+// var frutas = [{nome:"maçã", cor:"vermelha"}, {nome:"uva", cor:"roxa"}]
+// console.log(frutas);
+// alert(frutas[1].nome);
 
 
 // var fruta = {nome:"maçã", cor:"vermelha"}
 // console.log(fruta.nome);
 // alert(fruta.cor);
-
-
 
 
 // var lista = ["maça", "pera", "laranja"];
@@ -24,13 +54,10 @@ alert(frutas[1].nome);
 // console.log(lista.join(" - "));
 
 
-
-
 // var nome = "Lívia Pereira";
 // var n1 = 23;
 // var n2 = 10;
 // var frase = "Japão é o melhor time do mundo";
-
 
 
 // //alert(nome + " tem " + idade + " anos");
