e4e965e00fdeb2ccea736b4781ce58842041635e
Livia <liviapereira1997@hotmail.com>
2020-07-10 16:21:34 -0300

    practicing the basic of javascript

--- js/main.js
+++ js/main.js
@@ -1,12 +1,75 @@
 
-var d = new Date();
-//alert(d.getMonth()+1);
-//alert(d.getMinutes());
-//alert(d.getDay());
-alert(d.getHours());
+function clicou(){
+
+    document.getElementById("agradecimento").innerHTML = "Obrigado por clicar";
+    //console.log(document.getElementById("agradecimento"));
+    //alert ("Obrigado por clicar");
+}
+
+function redirecionar(){
+   // window.open("https://www.google.com/");
+    //window.location.href = "https://www.google.com/";
+}
+
+function trocar(elemento){
+    //document.getElementById("mousemove").innerHTML = "Obrigado por passar o mouse";
+    elemento.innerHTML = "Obrigado por passar o mouse";
+    //alert("trocar texto");
+
+}
+
+function voltar(elemento){
+    //document.getElementById("mousemove").innerHTML = "Passe o mouse aqui";
+    elemento.innerHTML = "Obrigado por passar o mouse";
+}
+
+function load(){
+    alert("página carregada");
+}
+
+function funcaoChange(elemento){
+    console.log(elemento.value);
+}
 
 
 
+// function soma(n1, n2){
+//     return n1 + n2;
+// }
+
+// function validaIdade(idade){
+//     var validar;
+//     if (idade >= 18){
+//         validar = true
+//     } else{
+//         validar = false
+//     }
+//     return validar;
+    
+// }
+
+// var idade = prompt("Qual sua idade?");
+// console.log(validaIdade(idade));
+
+
+
+//alert(soma(5, 10));
+
+// function setReplace(frase, nome, novo_nome){
+//     frase.replace(nome, novo_nome)
+// }
+// alert(setReplace("Vai Japão", "Japão", "Brasil"));
+
+
+
+
+
+
+// var d = new Date();
+// //alert(d.getMonth()+1);
+// //alert(d.getMinutes());
+// //alert(d.getDay());
+// alert(d.getHours());
 
 
 // var count;
